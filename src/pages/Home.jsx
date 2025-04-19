06b71e0860aa1050d09ae80d13981d576a89e6ee
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2025-04-19 14:21:46 -0300

    logica de home de versiones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,34 +4,63 @@ import Titulo from '../components/Titulo/Titulo'
 import Formulario from '../components/Formulario/Formulario'
 import Tarjeta from '../components/Tarjeta/tarjeton'
 
+let storage = localStorage.getItem("lista");
+storage = JSON.parse(storage)
 const Home = () => {
-    //vistas, no vistas?, formulario
-    //necesitamos una pagina para no vistas o seria parte de la logica del filtro?
-    const [pagina, setPagina] = useState("vistas");
-    const mostrarFormulario = () => {
-        pagina === "vistas" ?  setPagina("formulario")
-        : setPagina("vistas")
-        ;
-    }
+    //listar, vacio, formulario
+    const [pagina, setPagina] = useState("");
+
+    const cambiarEstado = () => {
+        if (pagina !== "formulario") {
+          setPagina("formulario");
+        } else if (storage === null) {
+          setPagina("vacio");
+        } else {
+          if (typeof storage === "string") {
+            storage = JSON.parse(storage);
+          }
+          setPagina("vistas");
+        }
+    }      
     
+    console.log(pagina)
+
+    useEffect(() => {//al iniciar la app decide estado inicial
+        if(storage === null){
+            setPagina("vacio")
+        }else{
+            setPagina("vistas")
+            if (typeof storage === "string") {//se ejecuta 2 veces todo el codigo no se porque
+                storage = JSON.parse(storage);
+            }
+        }
+      }, []);
     
-    let storage = localStorage.getItem("lista");
-    storage = JSON.parse(storage)
+
+   
     if(pagina === "formulario"){
         return (
             <div>
-                <Formulario click={mostrarFormulario}/>
+                <Formulario click={cambiarEstado}/>
             </div>
         )
     }else if(pagina === "vistas"){
         return(
             <div>
                 <Titulo titulo="TP1 PWA" />
-                <Boton texto="Cargar Pelicula" onClick={mostrarFormulario}/>    
+                <Boton texto="Cargar Pelicula" onClick={cambiarEstado}/>    
                 {!(storage === null) ? storage.map((peli, index) => (
                     <Tarjeta peliserie={peli} key={index}/>
                 )) : <p>No ha subido ninguna peliserie</p>} 
             </div>
+        )
+    }else{
+        return(
+        <div>
+            <Titulo titulo="TP1 PWA" />
+            <Boton texto="Cargar Pelicula" onClick={cambiarEstado}/>    
+            <p>No ha subido ninguna peliserie</p>
+        </div>
         )
     }
     
