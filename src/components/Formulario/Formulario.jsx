0aadb5d097814ce8e3f769d89c8b9b928141a1e9
Federico Lores FAI - 2948 <federico.lores@est.fi.uncoma.edu.ar>
2025-04-23 12:15:00 -0300

    refactorizado y estilo de formulario

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -39,13 +39,13 @@ const Formulario = ({ click }) => {
             <h2>Agregar Película/Serie</h2>
             <form onSubmit={handleSubmit}>
                     <label className={styles.formLabel}>Título:</label>
-                    <Input name="titulo" type="text" value={formData.titulo} onChange={handleChange} required />
+                    <Input name="titulo" type="text" value={formData.titulo} onChange={handleChange} required estilo={styles.input} />
                     <label className={styles.formLabel}>Director:</label>
-                    <Input name="director" type="text" value={formData.director} onChange={handleChange} required/>
+                    <Input name="director" type="text" value={formData.director} onChange={handleChange} required estilo={styles.input} />
                     <label className={styles.formLabel}>Año:</label>
-                    <Input name="anio" type="number" value={formData.anio} onChange={handleChange} />
+                    <Input name="anio" type="number" value={formData.anio} onChange={handleChange} estilo={styles.input} />
                     <label className={styles.formLabel}>Género:</label>
-                    <select name="genero" value={formData.genero} onChange={handleChange} >
+                    <select name="genero" value={formData.genero} onChange={handleChange} className={styles.select} >
                         <option value="terror">Terror</option>
                         <option value="drama">Drama</option>
                         <option value="fantasia">Fantasía</option>
@@ -54,16 +54,22 @@ const Formulario = ({ click }) => {
                         <option value="documental">Documental</option>
                     </select>
                     <label className={styles.formLabel}>Rating (1-10):</label>
-                    <Input name="rating" type="number" min="1" max="10" value={formData.rating} onChange={handleChange} />
-                    <p>Tipo:</p>
-                    <label htmlFor='tipo'>Pelicula</label>
-                    <Input type="radio" name="tipo" value="pelicula" checked={formData.tipo === "pelicula"} onChange={handleChange}/><br/>
-                    <label htmlFor='tipo'>Serie</label>
-                    <Input type="radio" name="tipo" value="serie" checked={formData.tipo === "serie"} onChange={handleChange}/>
+                    <Input name="rating" type="number" min="1" max="10" value={formData.rating} onChange={handleChange} className={styles.input} />        
+                    <div className={styles.radioContainer}>
+                        <p className={styles.radioTitle} >Tipo:</p>
+                        <div className={styles.radioGroup}>
+                            <Input type="radio" name="tipo" value="pelicula" id="nameP" checked={formData.tipo === "pelicula"} onChange={handleChange} estilo={styles.radioOptions} /><br/>
+                            <label htmlFor='tipoP' className={styles.radioLabel} >Pelicula</label>
+                            <Input type="radio" name="tipo" value="serie" id="nameS" checked={formData.tipo === "serie"} onChange={handleChange} estilo={styles.radioOptions} />
+                            <label htmlFor='tipoS' className={styles.radioLabel} >Serie</label>
+                        </div>
+                    </div>
                     <label className={styles.formLabel}>URL de la imagen:</label>
-                    <Input name="imagen" type="url" value={formData.imagen} onChange={handleChange} placeholder="https://ejemplo.com/imagen.jpg"/><br/>
-                    <Boton texto="Guardar" type="submit" />
-                    <Boton texto="Cancelar" type="button" onClick={click} />
+                    <Input name="imagen" type="url" value={formData.imagen} onChange={handleChange} placeholder="https://ejemplo.com/imagen.jpg" estilo={styles.input} /><br/>
+                    <div className={styles.buttonGroup} >
+                        <Boton texto="Guardar" type="submit" estilo={styles.primaryButton} />
+                        <Boton texto="Cancelar" type="button" onClick={click}  estilo={styles.secondaryButton} />
+                    </div>
             </form>
         </div>
     );
